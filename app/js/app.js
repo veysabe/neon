4fa3a011e72ccaf7acbb09d7833b0790838774a3
veysabe <veysabe@gmail.com>
2021-04-06 22:55:02 +0300

    git init

---
+++ app/js/app.js
@@ -0,0 +1,10 @@
+import $ from 'jquery'
+import slick from 'slick-slider'
+
+$('.main-block-slider').slick({
+    infinite: true,
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+});
